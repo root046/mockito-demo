be4f60cdb738649f405bd46eb9d655196295fa86
Bader Sanad Alanazi <alanazibs@rcjy.gov.sa>
2024-01-21 19:47:09 +0300

    Initial commit

---
+++ src/test/java/com/bader88/mockito/mockitodemo/MockitoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bader88.mockito.mockitodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockitoDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
